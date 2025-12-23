1d33648b696dd8b55d4073b2807b5b0a41f436fa
Leander Tops <tops.leander03@gmail.com>
2025-12-23 10:03:23 +0100

    Add FastAPI endpoints and CORS configuration

--- api/main.py
+++ api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import socket
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8080",
+        "http://localhost:8088"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/name")
 def get_name():
